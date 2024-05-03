0855522a1f551b20ab2c7a56dc091c5956ba58a2
VyacheslavGusev <117516863+VyacheslavGusev@users.noreply.github.com>
2024-05-03 18:03:12 +0300

    aggiorned func img

--- packeges/handlers/recive_file_router.py
+++ packeges/handlers/recive_file_router.py
@@ -19,6 +19,16 @@ async def download_photo(message: Message, state: FSMContext, bot: Bot):
     
     await message.answer("Файл получен, отправлен на распознание, подождите немного")
 
+    data = await state.get_data()
+    id_request = data.get('id_request')
+    compaign = data.get('compaign')
+    ed_inst = data.get('ed_inst')
+    name_ed_inst = data.get('name_ed_inst')
+    date = data.get('date')
+    city = data.get('city')
+    address = data.get('address')
+    responsible = data.get('responsible')
+
     file_path = f"tmp/{message.photo[-1].file_id}.jpg"
     output_images = f'tmp/{message.photo[-1].file_id}'
     Proc_image.file_to_png(file_path, output_images)
@@ -30,8 +40,9 @@ async def download_photo(message: Message, state: FSMContext, bot: Bot):
 
     result = YandexAPI.recognition_text( message.photo[-1].file_id)
     proc_data = Validation.read_json(result)
-    Validation.output_res(proc_data, message.photo[-1].file_id)
-
+    res_list, interest_list = Validation.output_res(proc_data, message.photo[-1].file_id)
+    Result.output_res(res_list, interest_list, message.photo[-1].file_id, id_request, compaign, ed_inst,\
+                      name_ed_inst, date, city, address, responsible)
     doc_output = FSInputFile(f"tmp/{message.photo[-1].file_id}/output.xlsx")
     await message.answer_document(doc_output, 
                                   caption="Результат распознания")
