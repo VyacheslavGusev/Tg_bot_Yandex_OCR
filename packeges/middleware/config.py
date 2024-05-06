576ed774b9ac825e899150eb48f419c55f5cbd15
VyacheslavGusev <117516863+VyacheslavGusev@users.noreply.github.com>
2024-05-06 13:07:09 +0300

    agg code final

--- packeges/middleware/config.py
+++ packeges/middleware/config.py
@@ -2,3 +2,9 @@ TELEGRAM_TOKEN = '7042345812:AAGDBQzNpghb5u5eFlmKvKGBtTcapCTzkg0'
 
 folder_id = "b1goutam1lu7mrpga53k"
 oauth_token = "y0_AgAAAAAE2_2tAATuwQAAAADyV6oXjMC0QTPRQhu7ZyDegvmNsI6tKWw"
+
+App_api_id = 27708648
+App_api_hash = '9eb11349bff5196e393dcc7763a3a4aa'
+Phone_number = '+79197480963'
+
+
